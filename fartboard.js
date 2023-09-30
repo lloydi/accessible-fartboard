f0b88514d74ce39351ee306c7b0cfc89dd879ab3
Ian Lloyd <github@lloydi.com>
2023-09-30 19:11:28 +0100

    Added a time delay option for sounds

--- fartboard.js
+++ fartboard.js
@@ -1,3 +1,4 @@
+let delayTime=0;
 function fartIt(whichFart){
  const audio = new Audio("farts/" + whichFart +".mp3");
  audio.play();
@@ -6,6 +7,11 @@ var fartButtons = document.querySelectorAll(".fart div.button, .fart button");
 Array.from(fartButtons).forEach(fartButton => {
  fartButton.addEventListener("click", e => {
   const whichFart = fartButton.getAttribute("id");
-  fartIt(whichFart);
+  checkDelay();
+  setTimeout(function(){fartIt(whichFart)}, (delayTime*1000));
  });
 });
+
+function checkDelay(){
+ delayTime = document.querySelector("[name='delay']:checked").value;
+}
